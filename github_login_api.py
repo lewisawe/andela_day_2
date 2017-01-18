a8e6e4862cf971977171b9e62426879b14312b78
Lewis Sawe <lewisbet9@gmail.con>
2017-01-18 16:33:47 +0300

    Gitthub Web API

---
+++ github_login_api.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def get_a_user(username, password):
+
+    # get data from github api using this request.
+    # change the user and pass string in the auth tuple with the desired credentials
+
+    req = requests.get('https://api.github.com/user', auth=(username, password))
+    if req.status_code == 200:
+        print("Username: " +req.json()["login"])
+        print("Account Created on: " +req.json()["created_at"])
+        print("Location: " +req.json()["location"])
+        return req.text
+    elif req.status_code == 404:
+        print(str(req.status_code)+" :User not found")
+        return "User not found"
+    elif req.status_code == 401:
+        print(str(req.status_code)+" :Wrong details, Access denied!")
+        return "Wrong details, Access denied!"
